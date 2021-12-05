c2c1c874bac87e6ebac5da734783f7da6af38808
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-12-05 11:27:07 -0300

    created the upvote route

--- src/controller/voteController.js
+++ src/controller/voteController.js
@@ -6,7 +6,6 @@ const upVote = async (req, res) => {
     const newVote = voteService.createVote({ id, type: 'upVote' });
     return res.status(201).send(newVote);
   } catch (error) {
-    console.log(error);
     return res.sendStatus(500);
   }
 };
