1f2d5934b602bd743597f8b0fa07df66b32be95d
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-12-05 10:57:48 -0300

    created a recommendation in the database

--- src/controller/recommendationController.js
+++ src/controller/recommendationController.js
@@ -8,10 +8,8 @@ const postRecommendation = async (req, res) => {
     if (!isValid) return res.sendStatus(400);
     const newRecommendation = await recommendationService.createReacommendations(recommendation);
     if (!newRecommendation) return res.sendStatus(400);
-    console.log(newRecommendation);
     return res.status(201).send(newRecommendation[0]);
   } catch (error) {
-    console.log(error);
     return res.sendStatus(500);
   }
 };
